affd0a155c3023f491fb90eb7b68c3ab1e527ee1
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-25 17:12:39 +0200

    build: reinstall packages

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AppLayout() {
+  return (
+    <View>
+      <Text>Hello from app/%28app%29/_layout.tsx</Text>
+    </View>
+  );
+}
