1719f78ca5c32bb28874654f216a2ea8cd9b0fa3
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-25 12:32:00 +0200

    build: set up `prettier` and clear code

--- app.config.ts
+++ app.config.ts
@@ -1,43 +1,44 @@
-import {ExpoConfig} from "@expo/config-types";
-import {ConfigContext} from "@expo/config";
+import { ExpoConfig } from "@expo/config-types";
+import { ConfigContext } from "@expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
-    ...config,    "name": "Workout Tracker",
-    "slug": "workout-tracker",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "workouttracker",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true
+  ...config,
+  name: "Workout Tracker",
+  slug: "workout-tracker",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "workouttracker",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff"
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      },
-      "edgeToEdgeEnabled": true
-    },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
-    },
-    "plugins": [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff"
-        }
-      ]
-    ],
-    "experiments": {
-      "typedRoutes": true
-    }
-  })
+    edgeToEdgeEnabled: true
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      }
+    ]
+  ],
+  experiments: {
+    typedRoutes: true
+  }
+});
--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -24,7 +24,7 @@ export default function Index() {
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "center"
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
@@ -42,7 +42,7 @@ export default function RootLayout() {
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
 const moveDirectories = async (userInput) => {
@@ -88,9 +88,7 @@ const moveDirectories = async (userInput) => {
     console.log("\n✅ Project reset complete. Next steps:");
     console.log(
       `1. Run \`npx expo start\` to start a development server.\n2. Edit app/index.tsx to edit the main screen.${
-        userInput === "y"
-          ? `\n3. Delete the /${exampleDir} directory when you're done referencing it.`
-          : ""
+        userInput === "y" ? `\n3. Delete the /${exampleDir} directory when you're done referencing it.` : ""
       }`
     );
   } catch (error) {
@@ -98,15 +96,12 @@ const moveDirectories = async (userInput) => {
   }
 };
 
-rl.question(
-  "Do you want to move existing files to /app-example instead of deleting them? (Y/n): ",
-  (answer) => {
-    const userInput = answer.trim().toLowerCase() || "y";
-    if (userInput === "y" || userInput === "n") {
-      moveDirectories(userInput).finally(() => rl.close());
-    } else {
-      console.log("❌ Invalid input. Please enter 'Y' or 'N'.");
-      rl.close();
-    }
+rl.question("Do you want to move existing files to /app-example instead of deleting them? (Y/n): ", (answer) => {
+  const userInput = answer.trim().toLowerCase() || "y";
+  if (userInput === "y" || userInput === "n") {
+    moveDirectories(userInput).finally(() => rl.close());
+  } else {
+    console.log("❌ Invalid input. Please enter 'Y' or 'N'.");
+    rl.close();
   }
-);
+});
