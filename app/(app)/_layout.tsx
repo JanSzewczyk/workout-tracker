2179d9fe564425ba94f4db8c3066be4af43ac4fe
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-26 17:45:53 +0200

    feat: add sign up screen

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,9 +1,27 @@
 import { Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+import { ActivityIndicator, View } from "react-native";
 
 export default function AppLayout() {
+  const { isSignedIn = false, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(auth)/sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/sign-up" options={{ headerShown: false }} />
+      </Stack.Protected>
     </Stack>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { Slot } from "expo-router";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+
+import "./global.css";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider>
+    <ClerkProvider tokenCache={tokenCache}>
       <Slot />
     </ClerkProvider>
   );
