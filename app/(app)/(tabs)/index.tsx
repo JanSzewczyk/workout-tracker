ca4b52296d6dce80fef7a650ffb1fb489c6f2f40
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-26 21:29:21 +0200

    feat: add sign out button to home screen

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { SignOutButton } from "@/components/sign-out-button";
 
 export default function HomeTab() {
   return (
     <SafeAreaView>
       <Text>Home Tab</Text>
+      <SignOutButton />
     </SafeAreaView>
   );
 }
