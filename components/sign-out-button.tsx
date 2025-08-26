99dfd20e9e0f4797e3f24affb68248550cc442c5
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-26 17:45:36 +0200

    feat: add create sign out button component

---
+++ components/sign-out-button.tsx
@@ -0,0 +1,23 @@
+import { useClerk } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
+import { Text, TouchableOpacity } from "react-native";
+
+export const SignOutButton = () => {
+  const { signOut } = useClerk();
+
+  async function handleSignOut() {
+    try {
+      await signOut();
+      await Linking.openURL(Linking.createURL("/"));
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }
+  return (
+    <TouchableOpacity onPress={handleSignOut}>
+      <Text>Sign out</Text>
+    </TouchableOpacity>
+  );
+};
