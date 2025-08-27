69ad33dbb068a41c5896373ecfc475ab724d4d4f
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-27 22:32:49 +0200

    feat: create otp schema

--- schemas/auth.ts
+++ schemas/auth.ts
@@ -5,3 +5,31 @@ export const loginSchema = z.object({
   password: z.string().min(8, "Password must be at least 8 characters")
 });
 export type LoginFormData = z.infer<typeof loginSchema>;
+
+export const signUpSchema = z
+  .object({
+    email: z.email("Please enter a valid email address").trim(),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must contain at least 8 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number"),
+    confirmPassword: z.string("Confirm Password is required").min(1, "Please confirm your password")
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"]
+  });
+export type SignUpFormData = z.infer<typeof signUpSchema>;
+
+export const otpSchema = z.object({
+  code: z
+    .string()
+    .min(1, "Verification code is required")
+    .length(6, "Code must be exactly 6 digits")
+    .regex(/^\d+$/, "Code must contain only numbers")
+    .transform((val) => val.trim())
+});
+export type OTPFormData = z.infer<typeof otpSchema>;
