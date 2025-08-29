a1c8b36cd6bda60686c8b131e3dfe9dff3e177e7
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-29 18:42:23 +0200

    feat: set up sanity client

---
+++ lib/sanity/client.ts
@@ -0,0 +1,22 @@
+import { ClientConfig, createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+const config = {
+  projectId: "aj5nqcwz",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: false
+} satisfies ClientConfig;
+
+export const client = createClient(config);
+
+const adminConfig = {
+  ...config,
+  token: process.env.EXPO_PULIC_SANITY_TOKEN
+} satisfies ClientConfig;
+
+export const adminClient = createClient(adminConfig);
+
+// Image URL builder
+const builder = imageUrlBuilder(config);
+export const urlFor = (source: string) => builder.image(source);
