c3b60fbd3625684a2d7908e8cf31ad52ed18641f
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-09-04 13:07:35 +0200

    feat(react-query): set up `react-query`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,17 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { Slot } from "expo-router";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { queryClient } from "~/lib/react-query/client";
 import "./global.css";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Slot />
+      <QueryClientProvider client={queryClient}>
+        <Slot />
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
