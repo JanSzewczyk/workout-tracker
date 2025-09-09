c0685623aca5c4f6a1eb18e1788cc1375d6ce0d7
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-09-09 23:42:38 +0200

    feat(workout): create workout store

---
+++ store/wokrout-store.ts
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export type WorkoutSet = {
+  id: string;
+  reps: string;
+  weight: string;
+  weightUnit: "kg" | "lbs";
+  isCompleted: boolean;
+};
+
+type WorkoutExercise = {
+  id: string;
+  sanityId: string;
+  name: string;
+  sets: Array<WorkoutSet>;
+};
+
+type WorkoutStore = {
+  workoutExercises: Array<WorkoutExercise>;
+  weightUnit: "kg" | "lbs";
+
+  addExerciseToWorkout: (exercise: { name: string; sanityId: string }) => void;
+  setWorkoutExercises: (
+    exercises: WorkoutExercise[] | ((prev: Array<WorkoutExercise>) => Array<WorkoutExercise>)
+  ) => void;
+  setWeightUnit: (unit: "kg" | "lbs") => void;
+  resetWorkout: () => void;
+};
+
+export const useWorkoutStore = create<WorkoutStore>()(
+  persist(
+    (set) => ({
+      workoutExercises: [],
+      weightUnit: "kg",
+
+      addExerciseToWorkout: (exercise) =>
+        set((state) => {
+          const newExercise: WorkoutExercise = {
+            id: Math.random().toString(),
+            sanityId: exercise.sanityId,
+            name: exercise.name,
+            sets: []
+          };
+          return { workoutExercises: [...state.workoutExercises, newExercise] };
+        }),
+      setWorkoutExercises: (exercises) =>
+        set((state) => ({
+          workoutExercises: typeof exercises === "function" ? exercises(state.workoutExercises) : exercises
+        })),
+      setWeightUnit: (unit) => set((state) => ({ weightUnit: unit })),
+      resetWorkout: () => set({ workoutExercises: [] })
+    }),
+    {
+      name: "workout-store",
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: (state) => ({
+        weightUnit: state.weightUnit
+      })
+    }
+  )
+);
